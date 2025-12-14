1efca2e1969bfdb550974ed47509c4608883db96
AkshayPadave <padaveakshay28@gmail.com>
2025-12-14 16:52:47 +0530

    submit answer api

--- quizsystem/quiz/views.py
+++ quizsystem/quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import Quiz
+from .models import Quiz, QuizResult, Question, Choice
 
 def take_quiz(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
@@ -11,4 +11,20 @@ def take_quiz(request, quiz_id):
 
 
 def submit_quiz(request, quiz_id):
-    return f"You have submitted quiz {quiz_id}"
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    questions = quiz.question_set.all()
+
+    total = 0
+
+    for q in questions:
+        user_answer = request.POST.get(str(q.id))
+        if q.question_type == 'mcq' or q.question_type == 'tf':
+            correct_choice = q.choices.filter(is_correct=True).first()
+            if correct_choice and user_answer == correct_choice.text:
+                total += 1
+        elif q.question_type == 'text':
+            if user_answer and user_answer.strip().lower() == "correct answer":
+                total += 1
+    
+    QuizResult.objects.create(quiz=quiz, score=total)
+    return render(request, 'quiz/quiz_result.html', {"quiz": quiz, "score": total})
